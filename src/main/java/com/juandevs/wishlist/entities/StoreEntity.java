dc1f93154faa1c3889bfa69d9893cdc4b343ace2
Juan Dev <juan.zapataoc@gmail.com>
2024-12-26 22:07:16 -0500

    entities finished

--- src/main/java/com/juandevs/wishlist/entities/StoreEntity.java
+++ src/main/java/com/juandevs/wishlist/entities/StoreEntity.java
@@ -1,8 +1,22 @@
 package com.juandevs.wishlist.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Entity
+@Table(name = "stores")
+@Getter
+@Setter
+@NoArgsConstructor
 public class StoreEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_store", nullable = false)
+    private Integer storeId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String address;
 }
--- src/main/java/com/juandevs/wishlist/entities/UserEntity.java
+++ src/main/java/com/juandevs/wishlist/entities/UserEntity.java
@@ -1,4 +1,26 @@
 package com.juandevs.wishlist.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Integer storeId;
+    @Column(name = "user_name", nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
 }
